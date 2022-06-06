31e30f9bff105a5a4ec9e8864b15d60b3b8ba19c
Shawon1997 <16haw87@gmail.com>
2022-06-06 16:19:05 +0530

    some change

--- src/components/CartPage.jsx
+++ src/components/CartPage.jsx
@@ -2,7 +2,7 @@
 import axios from "axios"
 import {useState ,useEffect} from 'react'
 
-import {Link} from "react-router-dom"
+//import {Link} from "react-router-dom"
 
 
 export const CartPage = () => {
